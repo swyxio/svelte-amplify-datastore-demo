e8fb69e9e521ecae5f2fcc90da0dd200c5d17b30
swyx <shawnthe1@gmail.com>
2020-01-23 17:30:55 -0500

    commit component based datastore impl

--- src/Main.svelte
+++ src/Main.svelte
@@ -1,45 +1,64 @@
 <script>
   import { store as authStore, logout } from './stores/auth.js'
-  import { store, addNote, searchNotes, listNotes, updateNote, deleteNote } from './stores/data.js'
-  import { onMount } from 'svelte';
-  import "bootstrap/dist/css/bootstrap.min.css";
+  import Amplify, { Hub } from '@aws-amplify/core';
+  import { DataStore, Predicates } from '@aws-amplify/datastore';
+  import { Note } from './models';
+  import { onMount, onDestroy } from 'svelte';
 
-  onMount(listNotes);
-  let notes = []
-  store.subscribe(_notes => void (notes = _notes))
-  let displayAdd = true
-  let displaySearch = false
-  let displayUpdate = false
+  let displayMode = 'add' // add, search, update
   let value = ""
-  let id = ""
   const resetValue = () => value = ""
-  let promise
-  const handleAddNote = () => void(promise = addNote(value).then(resetValue))
-  const handleUpdateNote = () => id !== "" && void (promise = updateNote())
+  let selectedId = ""
+  let notes = [] // in memory copy of all the results from DataStore queries
+  const setNotes = v => void (notes = v) // helper to set the store
+  onMount(listNotes);
+  const subscription = DataStore.observe(Note).subscribe(listNotes);
+  onDestroy(subscription.unsubscribe)
+  const handleConnectionChange = () => {
+    const condition = navigator.onLine ? 'online' : 'offline';
+    if (condition === 'online') listNotes();
+  };
+  window.addEventListener('online', handleConnectionChange);
+  Hub.listen('auth', (data) => (data.payload.event === 'signOut') && DataStore.clear())
+  function handleAddNote() {
+    return DataStore.save(new Note({ note: value })).then(listNotes).then(resetValue)
+  }
+  async function handleUpdateNote() {
+    const original = await DataStore.query(Note, selectedId);
+    await DataStore.save(
+      Note.copyOf(original, (updated) => void (updated.note = value))
+    ).then(listNotes)
+  }
   function handleSearch() {
-    displaySearch = true
-    promise = searchNotes(value)
+    displayMode = 'search'
+    DataStore.query(Note, (c) => c.note('contains', value)).then(setNotes)
   }
   function clearSearch() {
-    displaySearch = false;
+    displayMode = 'add'
     listNotes().then(resetValue)
   }
   async function handleSelect(note) {
     value = note.note
-    id = note.id
-    displayUpdate = true
-    displayAdd = false
+    selectedId = note.id
+    displayMode = 'update'
+  }
+  async function listNotes() {
+    return DataStore.query(Note, Predicates.ALL).then(setNotes);
+  }
+  function handleDelete(id) {
+    return async () => {
+      const toDelete = await DataStore.query(Note, id);
+      DataStore.delete(toDelete).then(listNotes)
+    }
   }
 </script>
 
 <h2>You are logged in as {$authStore.username} <button type="button" on:click={logout}>Log Out</button></h2>
-
 <div class="container">
-  {#if displayAdd}
+  {#if displayMode === 'add'}
   <form on:submit|preventDefault={handleAddNote}>
     <div class="input-group mb-3">
-      <input type="text" class="form-control form-control-lg" placeholder="New Note" aria-label="Note"
-        aria-describedby="basic-addon2" bind:value />
+      <input type="text" class="form-control form-control-lg" placeholder="New Note" bind:value />
       <div class="input-group-append">
         <button class="btn btn-warning border border-light text-white font-weight-bold" type="submit">
           Add Note
@@ -51,11 +70,10 @@
     </div>
   </form>
   {/if}
-  {#if displayUpdate}
+  {#if displayMode === 'update'}
   <form on:submit|preventDefault={handleUpdateNote} >
     <div class="input-group mb-3">
-      <input type="text" class="form-control form-control-lg" placeholder="Update Note" aria-label="Note"
-        aria-describedby="basic-addon2" bind:value />
+      <input type="text" class="form-control form-control-lg" placeholder="Update Note" bind:value />
       <div class="input-group-append">
         <button class="btn btn-warning text-white font-weight-bold" type="submit">
           Update Note
@@ -68,16 +86,13 @@
 <div class="container">
   {#each notes as item}
   <div class="alert alert-warning alert-dismissible text-dark show" role="alert">
-    <span on:click={() => handleSelect(item)}>
-      {item.note}
-    </span>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close" 
-      on:click={()=> {deleteNote(item.id).then(listNotes)}} >
+    <span on:click={() => handleSelect(item)}>{item.note}</span>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close" on:click={handleDelete(item.id)} >
       <span aria-hidden="true">&times;</span>
     </button>
   </div>
   {/each}
-  {#if displaySearch}
+  {#if displayMode === 'search'}
   <button class="button btn-warning float-right text-white font-weight-bold" on:click={clearSearch}>
     <span aria-hidden="true">Clear Search</span>
   </button>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.svelte';
 import Amplify from '@aws-amplify/core';
 import Auth from '@aws-amplify/auth';
 import aws_exports from './aws-exports';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // in this way you are only importing Auth and configuring it.
 Amplify.configure(aws_exports);
