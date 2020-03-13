6477ee28d564d13c31bb450a8d8150e6bbc9010f
swyx <shawnthe1@gmail.com>
2020-03-13 11:33:15 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps</p>
 	{#if $store != null}
 		<Main />
 	{:else}
